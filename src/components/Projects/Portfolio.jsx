1c7b73ac9d7cdf132af7a5b9c08a15168048d9eb
Mezouar Mohamed <mohmezpro@etik.com>
2024-11-07 13:54:22 +0100

    chemin image corriger

--- src/components/Projects/Portfolio.jsx
+++ src/components/Projects/Portfolio.jsx
@@ -1,46 +1,52 @@
 import './Portfolio.scss'; 
+import project1Image from '../../assets/images/Opera-Instantan2024-10-31150008www-imageonline.co-6068107.avif';
+import project2Image from '../../assets/images/Opera-Instantan2024-10-31151120www-imageonline.co-4892836.avif';
+import project3Image from '../../assets/images/Opera-Instantan2024-10-31165710nina-carducci-imageonline.co-5529075.avif';
+import project4Image from '../../assets/images/Opera-Instantan2024-10-31170743www-imageonline.co-6840371.avif';
+import project5Image from '../../assets/images/Opera-Instantan2024-10-31171933www-imageonline.co-5838837.avif';
+import project6Image from '../../assets/images/agile.webp';
 
 const projects = [
   {
     title: "Projet Booki",
     technology:"HTML et CSS, ",
     description: "JavaScript intégration de l'interface du site avec du code HTML et CSS",
-    imageUrl: "src/assets/images/Opera-Instantan2024-10-31150008www-imageonline.co-6068107.avif",
+    imageUrl: project1Image,
     repoLink: "https://github.com/mohpro7/PROJE_1_Creez_la_page_d-accueil_d-une_agence_de_voyage.git"
   },
   {
     title: "Projet Sophie Bluel",
     technology:"JavaScript",
     description: "création de pages web dynamiques avec JavaScript, communiquation avec des APIs",
-    imageUrl: "src/assets/images/Opera-Instantan2024-10-31151120www-imageonline.co-4892836.avif",
+    imageUrl: project2Image,
     repoLink: "https://github.com/mohpro7/Portfolio-architecte-sophie-bluel.git"
   },
   {
     title: "Projet Nina",
     technology:"SEOquake, Lighthouse, Wave,",
     description: "améliorer les performances d'un site, son accessibilité et son référencement",
-    imageUrl: "src/assets/images/Opera-Instantan2024-10-31165710nina-carducci-imageonline.co-5529075.avif",
+    imageUrl: project3Image,
     repoLink: "https://github.com/mohpro7/Nina-optimisation-2.git"
   },
   {
     title: "Projet kasa",
     technology:"Create React App, Node.js,",
     description: "Application utilisant React et React Router pour créer une expérience utilisateur moderne et réactive",
-    imageUrl: "src/assets/images/Opera Instantané_2024-10-31_170743_www.figma.com.png",
+    imageUrl: project4Image ,
     repoLink: "https://github.com/mohpro7/projet_kasa_vite.git"
   },
   {
     title: "Projet Mon Vieux Grimmoire",
     technology:"Node.js, Express, MongoDB, Mongoose",
     description: "Création d'un serveur afin de le connecter à une base de données, sécurisation des données et de leurs stockage, architecture MVC et création d'API RESTful",
-    imageUrl: "src/assets/images/Opera-Instantan2024-10-31171933www-imageonline.co-5838837.avif",
+    imageUrl: project5Image,
     repoLink: "https://github.com/mohpro7/P7-Dev-Web-livres.git"
   },
   {
     title: "Projet Planifiez le développement d'un site",
     technology:"PowerPoint, Asana, Word",
     description: "Mise en place d'une veille technologique, d'une fiche des spécifications techniques et gestion du projet avec Asana",
-    imageUrl: "src/assets/images/th (7).webp",
+    imageUrl: project6Image,
     repoLink: "https://github.com/mohpro7/planification-de-projet.git"
   }
 ];
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <p>&copy; {new Date().getFullYear()} Mohmez - Tous droits réservés</p>
         
         <a href="https://www.linkedin.com/in/ton-profil/" target="_blank" rel="noopener noreferrer" className="linkedin-link">
-            <img src="src\assets\images\linkedin-logo-png-2027-Windows.ico" alt="lien linkedin" />    
+            <img src="../../assets/images/linkedin-logo-png-2027-Windows.ico" alt="lien linkedin" />    
         </a>
       </div>
     </footer>
