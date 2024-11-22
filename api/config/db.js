323417c44187102a991a3834c6a6cdb45d3d9e83
Mezouar Mohamed <mohmezpro@etik.com>
2024-11-22 15:08:42 +0100

    suppresioncode obselet

--- api/config/db.js
+++ api/config/db.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  console.log('MONGO_URI:', process.env.MONGO_URI);
+  console.log('Chargement de MONGO_URI...');
+  console.log('MONGO_URI:', process.env.MONGO_URI || 'Variable non définie');
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
     console.log('Connexion à MongoDB réussie');
   } catch (err) {
-    console.error('Erreur de connexion à MongoDB', err);
+    console.error('Erreur de connexion à MongoDB', err.message);
     process.exit(1); // Arrête l'application si la connexion échoue
   }
 };
